2b51e7ce7784fc3d01c183839fddf36060959d39
Saad Ismail <saad.ismail@careem.com>
2020-07-25 17:42:46 +0500

    merge similar functionality

--- src/main/java/Greeter.java
+++ src/main/java/Greeter.java
@@ -35,43 +35,39 @@ public class Greeter {
 
     private String getUpperCaseNamesGreeting(List<String> names) {
         List<String> upperCaseNames = names.stream().filter(this::isUppercase).collect(Collectors.toList());
+
+        if (upperCaseNames.size() == 1) {
+            return "HELLO " + upperCaseNames.get(0) + "!";
+        }
+
         String upperCaseString = "";
         if (upperCaseNames.size() != 0) {
-            upperCaseString = getShoutedNames(upperCaseNames);
+            upperCaseString = String.format("HELLO, %s!", getJoinedNames(upperCaseNames));
         }
         return upperCaseString;
     }
 
     private String getNotUpperCaseNamesGreeting(List<String> names) {
         List<String> notUpperCaseNames = names.stream().filter(not(this::isUppercase)).collect(Collectors.toList());
+
+        if (notUpperCaseNames.size() == 1) {
+            return "Hello " + notUpperCaseNames.get(0) + ".";
+        }
+
         String notUpperCaseString = "";
         if (notUpperCaseNames.size() != 0) {
-            notUpperCaseString = getGreetedNames(notUpperCaseNames);
+            notUpperCaseString = String.format("Hello, %s.", getJoinedNames(notUpperCaseNames));
         }
         return notUpperCaseString;
     }
 
-    public String getShoutedNames(List<String> names)
+    public String getJoinedNames(List<String> names)
     {
-        if (names.size() == 1)
-            return "HELLO " + names.get(0) + "!";
-        else if (names.size() == 2)
-            return "HELLO, " + names.get(0) + " and " + names.get(1) + "!";
+        if (names.size() == 2)
+            return names.get(0) + " and " + names.get(1);
         else
         {
-            return "HELLO, " + getJoinedNames(names) + "!";
-        }
-    }
-
-    public String getGreetedNames(List<String> names)
-    {
-        if (names.size() == 1)
-            return "Hello " + names.get(0) + ".";
-        else if (names.size() == 2)
-            return "Hello, " + names.get(0) + " and " + names.get(1) + ".";
-        else
-        {
-            return "Hello, " + getJoinedNames(names) + ".";
+            return getJoinedNamesForMoreThanTwo(names);
         }
     }
 
@@ -80,7 +76,7 @@ public class Greeter {
     }
 
 
-    private String getJoinedNames(List<String> names) {
+    private String getJoinedNamesForMoreThanTwo(List<String> names) {
         int size = names.size();
         String lastName = names.get(size - 1);
         if (size == 1)
