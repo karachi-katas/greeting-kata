0236fafc0e9a022f53147e10c65539d84fa93b3d
Mehdi Raza Rajani <36185226+Rajani1998@users.noreply.github.com>
2020-07-25 16:20:25 +0500

    GREEN added code for multiple greetings (lower case)

--- src/main/java/Greet.java
+++ src/main/java/Greet.java
@@ -1,13 +1,37 @@
 public class Greet {
 
-    public String greet(String name) {
-        if (name == null) {
+//    public String greet(String name) {
+//        if (name == null) {
+//            return "Hello, my friend.";
+//        }
+//        if (isUpperCase(name)){
+//            return "HELLO " + name + "!";
+//        }
+//        return "Hello, " + name + ".";
+//    }
+
+    public String greet (String... names){
+
+        if (names == null) {
             return "Hello, my friend.";
         }
-        if (isUpperCase(name)){
-            return "HELLO " + name + "!";
+        if (names.length == 1 && isUpperCase(names[0])){
+            return "HELLO " + names[0] + "!";
         }
-        return "Hello, " + name + ".";
+
+        if (names.length == 1){
+            return "Hello, " + names[0] + ".";
+        }
+
+        StringBuilder greeting = new StringBuilder("Hello");
+
+        for (int i = 0; i < names.length - 1; i++){
+            greeting.append(", " +names[i]);
+        }
+
+        greeting.append(" and " + names[names.length-1] + '.');
+
+        return greeting.toString();
     }
 
     private boolean isUpperCase(String text) {
