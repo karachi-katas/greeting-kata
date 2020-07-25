820d631aec214f572b5d6e85845716448ec94d4c
Mehdi Raza Rajani <36185226+Rajani1998@users.noreply.github.com>
2020-07-25 17:37:29 +0500

    BLUE Refractoring

--- src/main/java/Greet.java
+++ src/main/java/Greet.java
@@ -1,11 +1,33 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.regex.Pattern;
+import java.util.*;
 
 public class Greet {
 
-    public String greet (String... names){
+    enum CASE {
+        UPPER, DEFAULT;
+    }
+
+    class Strategy {
+        String greet,
+                separator,
+                lastSeparator,
+                delimiter;
+
+        public Strategy(String greet, String separator, String lastSeparator, String delimiter) {
+            this.greet = greet;
+            this.separator = separator;
+            this.lastSeparator = lastSeparator;
+            this.delimiter = delimiter;
+        }
+    }
+
+    private static final Map<CASE, Strategy> strategies = new HashMap<>();
+
+    Greet() {
+        strategies.put(CASE.UPPER, new Strategy("HELLO", "", "AND", "!"));
+        strategies.put(CASE.DEFAULT, new Strategy("Hello", ",", "and", "."));
+    }
+
+    public String greet(String... names) {
 
         if (names == null) {
             return "Hello, my friend.";
@@ -13,55 +35,47 @@ public class Greet {
 
         names = preProcess(names);
 
-        if (names.length == 1 && isUpperCase(names[0])){
-            return "HELLO " + names[0] + "!";
-        }
-
-        if (names.length == 1){
-            return "Hello, " + names[0] + ".";
-        }
-
-        return greetMultipleMixCasesNames(names);
+        return greetNames(names);
     }
 
-    private String greetMultipleNamesDefaultCase(String[] names) {
-        StringBuilder greeting = new StringBuilder("Hello");
+    private String greetSingleName(String name) {
+        Strategy strategy = getStrategy(name);
+        return strategy.greet + strategy.separator + " " + name + strategy.delimiter;
+    }
 
-        for (int i = 0; i < names.length - 1; i++){
-            greeting.append(", " +names[i]);
+    private String greetMultipleNames(String[] names) {
+
+        if (names.length == 1) {
+            return greetSingleName(names[0]);
         }
 
-        greeting.append(" and " + names[names.length-1] + '.');
+        Strategy strategy = getStrategy(names[0]);
+
+        StringBuilder greeting = new StringBuilder(strategy.greet);
+
+
+        for (int i = 0; i < names.length - 1; i++) {
+            greeting.append(strategy.separator + " " + names[i]);
+        }
+
+        greeting.append(" " + strategy.lastSeparator + " " + names[names.length - 1] + strategy.delimiter);
 
         return greeting.toString();
     }
 
-    private String greetMultipleNamesUpperCase(String[] names) {
-
-        if (names.length == 1 && isUpperCase(names[0])){
-            return "HELLO " + names[0] + "!";
-        }
-
-        throw new UnsupportedOperationException();
-    }
-
-    private String greetMultipleMixCasesNames(String[] names) {
-        List<String> upperCaseNames = new ArrayList<String>();
-        List<String> defaultCaseNames = new ArrayList<String>();
+    private String greetNames(String[] names) {
+        List<String> upperCaseNames = getUpperCaseNames(names);
+        List<String> defaultCaseNames = getDefaultCaseNames(names);
         StringBuilder greeting = new StringBuilder();
-        for (int i = 0; i < names.length; i++){
-            if(isUpperCase(names[i])) {
-                upperCaseNames.add(names[i]);
-            } else {
-                defaultCaseNames.add(names[i]);
-            }
-        }
 
-        if(defaultCaseNames.size() > 0) {
-            greeting.append(greetMultipleNamesDefaultCase(defaultCaseNames.toArray(new String[0])));
+        if (isExists(defaultCaseNames)) {
+            greeting.append(greetMultipleNames(defaultCaseNames.toArray(new String[0])));
         }
-        if(upperCaseNames.size() > 0) {
-            greeting.append(" AND " + greetMultipleNamesUpperCase(upperCaseNames.toArray(new String[0])));
+        if (isExists(defaultCaseNames) && isExists(upperCaseNames)) {
+            greeting.append(" AND ");
+        }
+        if (isExists(upperCaseNames)) {
+            greeting.append(greetMultipleNames(upperCaseNames.toArray(new String[0])));
         }
         return greeting.toString();
     }
@@ -70,14 +84,13 @@ public class Greet {
         return text.equals(text.toUpperCase());
     }
 
-    private String[] preProcess(String... names){
-        List<String> processedNames = new ArrayList<String>();
+    private String[] preProcess(String... names) {
+        List<String> processedNames = new ArrayList<>();
 
-        for (String name : names){
-            if (name.contains(",")){
+        for (String name : names) {
+            if (name.contains(",")) {
                 processedNames.addAll(Arrays.asList(name.split(",\\s*")));
-            }
-            else {
+            } else {
                 processedNames.add(name.trim());
             }
         }
@@ -85,5 +98,34 @@ public class Greet {
         return processedNames.toArray(new String[0]);
     }
 
+    private Strategy getStrategy(String text) {
+        if (isUpperCase(text))
+            return strategies.get(CASE.UPPER);
+        return strategies.get(CASE.DEFAULT);
+    }
+
+    private List<String> getUpperCaseNames(String... names) {
+        List<String> upperCaseNames = new ArrayList<>();
+        for (String name : names) {
+            if (isUpperCase(name)) {
+                upperCaseNames.add(name);
+            }
+        }
+        return upperCaseNames;
+    }
+
+    private List<String> getDefaultCaseNames(String... names) {
+        List<String> defaultCaseNames = new ArrayList<>();
+        for (String name : names) {
+            if (!isUpperCase(name)) {
+                defaultCaseNames.add(name);
+            }
+        }
+        return defaultCaseNames;
+    }
+
+    private boolean isExists(List<String> array) {
+        return array.size() > 0;
+    }
 
 }
