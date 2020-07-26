5ab8042bec6722aa62a5c567b7f39289f644fd0b
lamak <lamak.qaizar@careem.com>
2020-07-26 16:50:30 +0500

    refacted is shout method

--- src/main/java/Greeting.java
+++ src/main/java/Greeting.java
@@ -4,10 +4,14 @@ public class Greeting {
         if (guest(name)) {
             return "Hello, my friend.";
         }
-        if (name.equals(name.toUpperCase())) {
+        if (shout(name)) {
             return String.format("HELLO %s!", name);
         }
         return String.format("Hello, %s.", name);
+    }
+
+    private boolean shout(String name) {
+        return name.equals(name.toUpperCase());
     }
 
     private boolean guest(String name) {
