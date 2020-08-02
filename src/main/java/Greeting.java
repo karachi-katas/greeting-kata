81440a9a86d902b4fb5b914a393f0fb1fdbc1ec3
lamak <lamak.qaizar@careem.com>
2020-08-02 16:02:32 +0500

    refactored into smaller methods and constants

--- src/main/java/Greeting.java
+++ src/main/java/Greeting.java
@@ -2,58 +2,83 @@ import java.util.Arrays;
 
 public class Greeting {
 
+    public static final String GUEST_GREETING = "Hello, my friend.";
+    public static final String SHOUT_GREETING = "HELLO {name}!";
+    public static final String NORMAL_GREETING = "Hello, {name}.";
+    public static final String SHOUT_AND = "{firstName} AND {secondName}";
+    public static final String NORMAL_AND = "{firstName} and {secondName}";
+
     private String greetOne(String name) {
-        if (guest(name)) {
-            return "Hello, my friend.";
-        }
         if (shoutAt(name)) {
-            return "HELLO {name}!".replace("{name}", name);
+            return SHOUT_GREETING.replace("{name}", name);
         }
-        return "Hello, {name}.".replace("{name}", name);
+        return NORMAL_GREETING.replace("{name}", name);
     }
 
     private String greetTwo(String firstName, String secondName) {
         if (shoutAt(firstName)) {
-            return greetOne("{name1} AND {name2}"
-                    .replace("{name1}", firstName)
-                    .replace("{name2}", secondName));
+            return greetOne(SHOUT_AND
+                    .replace("{firstName}", firstName)
+                    .replace("{secondName}", secondName));
         }
-        return greetOne("{name1} and {name2}"
-                .replace("{name1}", firstName)
-                .replace("{name2}", secondName));
+        return greetOne(NORMAL_AND
+                .replace("{firstName}", firstName)
+                .replace("{secondName}", secondName));
     }
 
     private boolean shoutAt(String name) {
         return name.toUpperCase().equals(name);
     }
 
-    private boolean guest(String name) {
-        return name == null;
+    private boolean guest(String[] names) {
+        return names == null;
     }
 
     public String to(String... names) {
-        if (names == null) {
-            return "Hello, my friend.";
+        if (guest(names)) {
+            return GUEST_GREETING;
         }
 
         if (names.length == 1) {
             return greetOne(names[0]);
         }
 
-        if (Arrays.stream(names).anyMatch(name -> name.toUpperCase().equals(name)) &&
-                Arrays.stream(names).filter(name -> name.toUpperCase().equals(name)).count() < names.length) {
-            String[] upperCaseNames = Arrays.stream(names).filter(name -> name.toUpperCase().equals(name)).toArray(String[]::new);
-            String[] lowerCaseNames = Arrays.stream(names).filter(name -> !name.toUpperCase().equals(name)).toArray(String[]::new);
-            return "{lowerCase} AND {upperCase}"
-                    .replace("{lowerCase}", to(lowerCaseNames))
-                    .replace("{upperCase}", to(upperCaseNames));
+        if (mixOfUpperAndLowerCase(names)) {
+            String[] upperCaseNames = filterUpperCase(names);
+            String[] lowerCaseNames = filterLowerCase(names);
+            return "{lowerCaseNames} AND {upperCaseNames}"
+                    .replace("{lowerCaseNames}", to(lowerCaseNames))
+                    .replace("{upperCaseNames}", to(upperCaseNames));
         }
 
         if (names.length >= 3) {
-            String commaSeparatedNames = String.join(", ", Arrays.copyOfRange(names, 0, names.length - 1));
-            return to(commaSeparatedNames + ",", names[names.length-1]);
+            String joinedNames = join(names);
+            return to(joinedNames, lastOf(names));
         }
 
         return greetTwo(names[0], names[1]);
+    }
+
+    private String[] filterLowerCase(String[] names) {
+        return Arrays.stream(names).filter(name -> !name.toUpperCase().equals(name)).toArray(String[]::new);
+    }
+
+    private String[] filterUpperCase(String[] names) {
+        return Arrays.stream(names).filter(name -> name.toUpperCase().equals(name)).toArray(String[]::new);
+    }
+
+    private String lastOf(String[] names) {
+        return names[names.length-1];
+    }
+
+    private String join(String[] names) {
+        String commaSeparatedNames = String.join(", ",
+                Arrays.copyOfRange(names, 0, names.length - 1));
+        return commaSeparatedNames + ",";
+    }
+
+    private boolean mixOfUpperAndLowerCase(String[] names) {
+        return Arrays.stream(names).anyMatch(name -> name.toUpperCase().equals(name)) &&
+                Arrays.stream(names).filter(name -> name.toUpperCase().equals(name)).count() < names.length;
     }
 }
